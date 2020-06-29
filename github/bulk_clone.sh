ce72337b949f6300e45cb58ae8529fabb35d8252
micnncim <micnncim@gmail.com>
2020-06-30 01:03:51 +0900

    Refactor github/bulk_clone.sh

--- github/bulk_clone.sh
+++ github/bulk_clone.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+readonly ESC=$(printf '\033')
+readonly RED="${ESC}[31m"
+readonly BLUE="${ESC}[34m"
+readonly NO_COLOR="${ESC}[m"
+
+readonly GITHUB_API="https://api.github.com"
+
+owner=$1
+if [[ -z "${owner}" ]]; then
+  error "missing argument: owner"
+  usage
+  exit 1
+fi
+
+info() {
+  echo "${BLUE}$(date +'%Y-%m-%dT%H:%M:%S%z')  INFO $*${NO_COLOR}" >&2
+}
+
+error() {
+  echo "${RED}$(date +'%Y-%m-%dT%H:%M:%S%z') ERROR $*${NO_COLOR}" >&2
+}
+
 usage() {
   cat <<EOF
 
@@ -17,53 +39,52 @@ usage() {
 EOF
 }
 
-GITHUB_API="https://api.github.com"
-
-cmd='ghq get'
-
-# Confirm whether ghq is installed or not
-if ! type ghq &>/dev/null; then
-  echo "[INFO] x-motemen/ghq is not installed. git will be used instead"
-  cmd='git clone'
-  exit 1
-fi
-
-# Confirm whether git is installed or not
-if ! type git &>/dev/null; then
-  echo "[ERROR] git is not installed"
-  exit 1
-fi
-
-# Confirm whether jq is installed or not
-if ! type jq &>/dev/null; then
-  echo "[ERROR] jq is not installed"
-  exit 1
-fi
-
-owner=$1
-if [[ -z "${owner}" ]]; then
-  echo "[ERROR] missing owner" >&2
-  usage
-  exit 1
-fi
-
-SECONDS=0
-page=1
-
-while [[ SECONDS -lt 300 ]]; do # timeout after 5m
-  resp=$(curl -sL "${GITHUB_API}/users/${owner}/repos?type=sources&page=${page}")
-
-  if [[ $(echo "${resp}" | jq '. | length') -eq 0 ]]; then
-    exit 0
+check_executables() {
+  # Confirm whether git is installed or not
+  if ! type git &>/dev/null; then
+    error "git is not installed"
+    exit 1
   fi
 
-  urls=$(echo "${resp}" | jq -r '.[].html_url')
+  # Confirm whether jq is installed or not
+  if ! type jq &>/dev/null; then
+    error "jq is not installed"
+    exit 1
+  fi
+}
 
-  for url in $urls; do
-    eval "${cmd} ${url}"
+clone_repos() {
+  SECONDS=0
+  page=1
+
+  while [[ SECONDS -lt 300 ]]; do # timeout after 5m
+    resp=$(curl -sL "${GITHUB_API}/users/${owner}/repos?type=sources&page=${page}")
+
+    if [[ $(echo "${resp}" | jq '. | length') -eq 0 ]]; then
+      exit 0
+    fi
+
+    urls=$(echo "${resp}" | jq -r '.[].html_url')
+
+    for url in $urls; do
+      info "Cloning ${url#'https://github.com/'}..."
+
+      if type ghq &>/dev/null; then
+        ghq get "${url}"
+      else
+        git clone "${url}"
+      fi
+    done
+
+    page=$((page + 1))
   done
 
-  page=$((page + 1))
-done
+  error "execution timeout"
+}
 
-echo "[ERROR] execution timeout"
+main() {
+  check_executables
+  clone_repos
+}
+
+main "$@"
