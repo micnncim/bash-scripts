42234c6462390fa590407da9ebf4cc2a4fe7d46d
micnncim <micnncim@gmail.com>
2020-07-02 03:03:58 +0900

    Refactor github/bulk_clone.sh

--- github/bulk_clone.sh
+++ github/bulk_clone.sh
@@ -5,9 +5,8 @@ set -o pipefail
 
 project_root_dir="$(git rev-parse --show-toplevel)"
 # shellcheck source=../lib/lib.sh
+# shellcheck disable=SC1091
 source "${project_root_dir}/lib/lib.sh"
-
-readonly GITHUB_API="https://api.github.com"
 
 usage() {
   cat <<EOF
@@ -27,46 +26,22 @@ EOF
 }
 
 check_executables() {
-  # Confirm whether git is installed or not
-  if ! type git &>/dev/null; then
-    log::error "git is not installed"
-    exit 1
-  fi
-
-  # Confirm whether jq is installed or not
-  if ! type jq &>/dev/null; then
-    log::error "jq is not installed"
-    exit 1
-  fi
+  tools=(git jq)
+  for tool in "${tools[@]}"; do
+    util::check_executable "${tool}"
+  done
 }
 
 clone_repos() {
-  SECONDS=0
-  page=1
+  for url in $(gh api --paginate 'users/:owner/repos?sort=updated&direction=desc' | jq -r '.[].html_url'); do
+    log::info "Cloning ${url#'https://github.com/'}..."
 
-  while [[ SECONDS -lt 300 ]]; do # timeout after 5m
-    resp=$(curl -sL "${GITHUB_API}/users/${owner}/repos?type=sources&page=${page}")
-
-    if [[ $(echo "${resp}" | jq '. | length') -eq 0 ]]; then
-      exit 0
+    if type ghq &>/dev/null; then
+      ghq get -u -s "${url}" || true
+    else
+      git clone --quiet "${url}" || true
     fi
-
-    urls=$(echo "${resp}" | jq -r '.[].html_url')
-
-    for url in $urls; do
-      log::info "Cloning ${url#'https://github.com/'}..."
-
-      if type ghq &>/dev/null; then
-        ghq get "${url}"
-      else
-        git clone "${url}"
-      fi
-    done
-
-    page=$((page + 1))
   done
-
-  log::error "execution timeout"
 }
 
 main() {
@@ -77,8 +52,10 @@ main() {
     exit 1
   fi
 
-  util::ask "Trying to clone ${owner}'s all the repostories. Proceed?" || exit 0
   check_executables
+
+  util::ask "Trying to clone ${owner}'s all the repostories. Proceed?" || exit 0
+
   clone_repos
 }
 
